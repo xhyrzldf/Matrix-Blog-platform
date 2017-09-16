8982f4590d029ce9b12958b5318dbd3a7a485d36
xhyrzldf <296796683@qq.com>
2017-09-17 00:56:26 +0800

    添加了controller层

---
+++ src/main/java/com/matrix/spring/boot/esblog/controller/BlogController.java
@@ -0,0 +1,42 @@
+package com.matrix.spring.boot.esblog.controller;
+
+import com.matrix.spring.boot.esblog.domain.es.EsBlog;
+import com.matrix.spring.boot.esblog.repository.es.EsBlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Author 李迪凡[xhyrzldf@foxmail.com]
+ * @Date 2017/9/17 0:37
+ */
+@RestController
+@RequestMapping("/blogs")
+public class BlogController {
+
+    @Autowired
+    private EsBlogRepository esBlogRepository;
+
+
+    @GetMapping
+    public List<EsBlog> list(@RequestParam(value = "title") String title,
+                             @RequestParam(value = "summary") String summary,
+                             @RequestParam(value = "content") String content,
+                             @RequestParam(value = "pageIndex", defaultValue = "0") int pageIndex,
+                             @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+
+        Pageable pageable = new PageRequest(pageIndex, pageSize);
+        Page<EsBlog> page = esBlogRepository.findDistinctEsBlogByTitleContainingOrSummaryContainingOrContentContaining(title, summary, content, pageable);
+
+        return page.getContent();
+    }
+
+}
